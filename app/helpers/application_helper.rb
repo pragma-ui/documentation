12fe1ba900f8b0dc19804a1321a1e52fbf0248be
Dhairya Gabhawala <gabhawaladhairya@gmail.com>
2021-09-30 23:17:10 -0500

    Add favicon, page titles, and performance improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def page_title
+		return "#{action_name.capitalize} - Pragma UI" if ["documentation", "developers", "examples", "themes"].include?(action_name)
+		return "#{params[:component].capitalize} - Components - Pragma UI" if params[:component]
+		"Pragma UI"
+	end
 end
