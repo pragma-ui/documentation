b25a3370c521644bd8221404aec1bf56a1ce6bdc
Dhairya Gabhawala <99418380+dhairya-gabhawala@users.noreply.github.com>
2022-09-26 16:38:38 -0500

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,11 +26,6 @@ require("notifications")
 document.addEventListener('turbolinks:load', function () {
   'use strict';
 
-  //
-  // Performs a "Cutting the mustard" test. If the browser supports the features
-  // tested, adds a mdl-js class to the <html> element. It then upgrades all MDL
-  // components requiring JavaScript.
-  //
   if ('classList' in document.createElement('div') &&
    'querySelector' in document &&
    'addEventListener' in window && Array.prototype.forEach) {
